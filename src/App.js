855e5c6890dd2357a030f4e17b7e7b0aa593a368
wadecodes <wadecodes@gmail.com>
2020-09-14 17:14:06 +0530

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from 'react';
 
 import HomePage from './pages/homepage/homepage.component';
 
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 const App = () => {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      </Switch>
     </div>
   );
 };
 
 export default App;
-
