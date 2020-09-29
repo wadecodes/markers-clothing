5a7c0039a6193538500b2e06c3899980c834ee35
wadecodes <wadecodes@gmail.com>
2020-09-29 13:44:03 +0530

    removing redux dev from production mode

--- src/store.js
+++ src/store.js
@@ -5,6 +5,12 @@ import reducers from './reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export const store = createStore(reducers, composeEnhancers(applyMiddleware()));
+let storeConfig;
+if (process.env.NODE_ENV === 'development') {
+  storeConfig = createStore(reducers, composeEnhancers(applyMiddleware()));
+} else {
+  storeConfig = createStore(reducers);
+}
+export const store = storeConfig;
 
 export const persistor = persistStore(store);
